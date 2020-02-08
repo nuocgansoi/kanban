10a2df0c7d5b8d45f0f7a6c9c7b274c51faeaa7f
Đặng Quốc Đạt <nuocgansoi@gmail.com>
2020-02-08 11:09:47 +0700

    Update journal_controller.rb

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -4,27 +4,27 @@ class JournalController < ApplicationController
   end
   
   #
-  # ジャーナルを取得する
+  # Nhận tạp chí
   #
   def get_journal
-    # POST値を取得
+    # POST Nhận giá trị
     card_id = params[:card_id]
     
-    # チケットIDを取得する
+    # Nhận ID thẻ
     issue_array = card_id.split("-")
     issue_id = issue_array[1].to_i
 
-    # 返却値
+    # Giá trị trả về
     result_hash = {}
 
-    # チケットの取得
+    # Nhận thẻ
     issue = Issue.find(issue_id);
 
-    # ヘッダ表示
+    # Hiển thị tiêu đề
     notes_string ="<p><b>#" + issue_id.to_s + "</b></p>"
     notes_string ="<a href=\"../issues/" + issue_id.to_s + "\"><p><b>#" + issue_id.to_s + "</b><p></a>"
 
-    # 説明欄
+    # Cột mô tả
     if !issue.description.blank? then
       user = User.find(issue.author_id)
       notes_string += "<table class=\"my-journal-table\">"
@@ -35,7 +35,7 @@ class JournalController < ApplicationController
       notes_string += "</a>"
       notes_string += "　"
       notes_string += user.lastname
-      notes_string += "さん"
+      notes_string += ""
       notes_string += "</th>"
       notes_string += "</tr>"
       notes_string += "<tr>"
@@ -46,7 +46,7 @@ class JournalController < ApplicationController
       notes_string += "</table>"
     end
 
-    # ノートを取得（新しいものから3件）
+    # Nhận ghi chú (3 mới nhất)
     notes = Journal.where(journalized_id: issue_id)
                    .where(journalized_type: "Issue")
                    .where(private_notes: 0)
@@ -55,12 +55,12 @@ class JournalController < ApplicationController
                    .limit(3)
                    .order("created_on DESC")
 
-    # ヘッダ表示
+    # Hiển thị tiêu đề
     if !notes.blank? then
-      notes_string +="<p><b>最新の履歴</b></p>"
+      notes_string +="<p><b>Lịch sử mới nhất</b></p>"
     end
 
-    # ノートをTABLEで組む
+    # Tạo ghi chú với BẢNG
     notes.reverse_each {|note|
       if !note.notes.blank? then
         user = User.find(note.user_id)
@@ -72,7 +72,7 @@ class JournalController < ApplicationController
         notes_string += "</a>"
         notes_string += "　"
         notes_string += user.lastname
-        notes_string += "さん"
+        notes_string += ""
         notes_string += "</th>"
         notes_string += "</tr>"
         notes_string += "<tr>"
@@ -84,21 +84,21 @@ class JournalController < ApplicationController
       end
     }
 
-    # コメント投稿フォーム
-    notes_string += "<p><b>コメント投稿</b></p>"
+    # Mẫu bình luận
+    notes_string += "<p><b>Gửi ý kiến</b></p>"
     notes_string += "<table class=\"my-comment-table\"><tr><td>"
     notes_string += "<textarea id=\"comment_area\" class=\"my-comment-textarea\" rows=\"5\"></textarea>"
-    notes_string += "<p><input type=\"button\" id=\"submit-journal-button\" value=\"送信\"></p>"
+    notes_string += "<p><input type=\"button\" id=\"submit-journal-button\" value=\"Gửi\"></p>"
     notes_string += "</td></tr></table>"
     
-    # 返却
+    # Trở về
     result_hash["result"] = "OK"
     result_hash["notes"] = notes_string
     render json: result_hash
   end
 
   #
-  # ノートを省略する
+  # Bỏ qua ghi chú
   #
   def trim_notes(notes)
     str = notes.byteslice(0, Constants::MAX_NOTES_BYTESIZE).scrub('')
@@ -109,28 +109,28 @@ class JournalController < ApplicationController
   end
 
   #
-  # ジャーナルを追加する
+  # Thêm tạp chí
   #
   def put_journal
-    # POST値を取得
+    # POST Nhận giá trị
     card_id = params[:card_id]
     note = params[:note]
     
-    # 返却値
+    # Giá trị trả về
     result_hash = {}
 
-    # チケットIDを取得する
+    # Nhận ID thẻ
     issue_array = card_id.split("-")
     issue_id = issue_array[1].to_i
 
-    # チケット取得
+    # Nhận thẻ
     issue = Issue.find(issue_id)
 
-    # ノートを追加する
+    # Thêm ghi chú
     note = Journal.new(:journalized => issue, :notes => note, user: User.current)
     note.save!
 
-    # 返却
+    # Trở về
     result_hash["result"] = "OK"
     render json: result_hash
   end
